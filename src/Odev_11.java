42a1415ac0909c9a528ed9a5792d02971a68797d
Onur Mergen <mergenonur@gmail.com>
2024-05-28 22:54:55 +0300

    G

--- src/Odev_11.java
+++ src/Odev_11.java
@@ -32,12 +32,13 @@ SELECT first_name FROM customer
 ORDER BY first_name;
 
 2.
-SELECT actor.first_name FROM actor
-JOIN customer ON actor.first_name = customer.first_name
-ORDER BY actor.first_name;
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+ORDER BY first_name;
 
 3.
-SELECT actor.first_name FROM actor
-LEFT JOIN customer ON actor.first_name = customer.first_name
-WHERE customer.first_name IS NULL
-ORDER BY actor.first_name;
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer
+ORDER BY first_name;
